64f8e5a09aa982fb7c50c10340eb913a7b7b2452
sasha2804 <o.korotushko@gmail.com>
2022-07-12 17:36:42 +0200

    1 commit

---
+++ 1_day.py
@@ -0,0 +1,3 @@
+in1 = input("Welcome to the Band Name Generator \nWhat is name of the city you grew up in? ")
+in2 = input("What is your pet's name? ")
+print("Your band name can be either %s or %s"%(in1, in2))
