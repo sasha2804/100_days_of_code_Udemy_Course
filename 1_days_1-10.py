56b7a0dfe679dcb0b57e92c47e2d00c0e6352625
sasha2804 <O.Korotushko@gmail.com>
2022-07-31 16:48:50 +0200

    new file

--- 1_days_1-10.py
+++ 1_days_1-10.py
@@ -425,40 +425,74 @@
 
 
 # method 2
-import random
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+# import random
+# letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+# numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+# symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-print("Welcome to the PyPassword Generator!")
-nr_letters= int(input("How many letters would you like in your password?\n")) 
-nr_symbols = int(input(f"How many symbols would you like?\n"))
-nr_numbers = int(input(f"How many numbers would you like?\n"))
+# print("Welcome to the PyPassword Generator!")
+# nr_letters= int(input("How many letters would you like in your password?\n")) 
+# nr_symbols = int(input(f"How many symbols would you like?\n"))
+# nr_numbers = int(input(f"How many numbers would you like?\n"))
 
-passList = []
-passString = ""
+# passList = []
+# passString = ""
 
-for i in range(nr_letters):
-    passList.append(random.choice(letters))
+# for i in range(nr_letters):
+#     passList.append(random.choice(letters))
 
-for i in range(nr_numbers):
-    passList.append(random.choice(numbers))
+# for i in range(nr_numbers):
+#     passList.append(random.choice(numbers))
 
-for i in range(nr_symbols):
-    passList.append(random.choice(symbols))
+# for i in range(nr_symbols):
+#     passList.append(random.choice(symbols))
 
-random.shuffle(passList)
+# random.shuffle(passList)
 
-for i in passList:
-    passString += i
+# for i in passList:
+#     passString += i
 
-print(passString)
-
-
+# print(passString)
 
 
+#   DAY 7 
+# import random
+# from hangman_words import word_list
+# from hangman_art import stages
+# from hangman_art import logo
+
+# #Step 1 
+# endOfGame = False
+# qtyOfLives = 6
+
+# print(logo)
+# #TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+# chosenWord = random.choice(word_list)
+# print(f"Chosen word is {chosenWord}")
+# #TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+# listGuesLett = ["_" for i in range(len(chosenWord))]
+
+# while not endOfGame:
+#     guessLetter = input("Make a guess letter ").lower()
+#     for i in range (len(chosenWord)):
+#         letter = chosenWord[i]
+#         if guessLetter == letter:
+#             listGuesLett[i] = guessLetter
+    
+#     if guessLetter not in chosenWord:
+#         qtyOfLives -= 1
+#         if qtyOfLives == 0:
+#             endOfGame = True
+#             print("You lost")
+#     print(f" ".join(listGuesLett))
+
+#     if "_" not in listGuesLett:
+#         endOfGame = True
+#         print("You won.")
+#     print(stages[qtyOfLives])
 
 
 
 
-
+        
+     
