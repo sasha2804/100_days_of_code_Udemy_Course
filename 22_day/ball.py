af3e8eed1380cb3fc2d20aafd85820239cf95be6
sasha2804 <o.korotushko@gmail.com>
2022-10-19 10:55:49 +0200

    123

--- 22_day/ball.py
+++ 22_day/ball.py
@@ -1,18 +1,43 @@
 import math
 
-a = 25
-b = 45
-result = math.atan(a/b)
-print(math.degrees(result))
+# a = 25
+# b = 45
+# result = math.atan(a/b)
+# print(math.degrees(result))
 
 from turtle import Turtle
 
 class Ball(Turtle):
     def __init__(self):
+        self.start = (0,0)
+        self.end = (150,150)
+        # self.heading = 45        
         super().__init__()
         self.shape('circle')
         self.color('white')
+        self.penup()
         self.turtlesize(1,1)
         self.goto(0,0)
+        self.angle = self.calc_heading()
+        self.setheading(self.angle)
+  
+        
+       
+
+
+
+    def move(self):        
+        self.forward(2)
+        print(self.heading())
+
+    def calc_heading(self):
+        delta_x = self.end[0] - self.start[0]
+        delta_y = self.end[1] - self.start[1]
+        result = math.atan(delta_y/delta_x)
+        print('result',math.degrees(result))
+        return int(math.degrees(result))
+
+
+
 
 
--- 22_day/main.py
+++ 22_day/main.py
@@ -1,6 +1,8 @@
+import imp
 from turtle import Screen, Turtle
 from paddles import Paddle, Net
 from ball import Ball
+import time
 
 screen = Screen()
 screen.setup(width=800, height=600)
@@ -14,9 +16,18 @@ paddle_right = Paddle((350,0))
 net = Net()
 ball = Ball()
 
+
+
+
+
 game_is_on = True
 
+# for i in range(150):
+#     ball.move()
+
 while game_is_on:
+    time.sleep(0.01)
+    ball.move()
     screen.update()
     screen.onkey(paddle_right.move_up,'Up')
     screen.onkey(paddle_right.move_down,'Down')
@@ -24,5 +35,9 @@ while game_is_on:
     screen.onkey(paddle_left.move_down,'Y')
     screen.onkey(paddle_left.move_up, 'a')
     screen.onkey(paddle_left.move_down,'y')
+
+    # ball.move()
+
+
 
 screen.exitonclick()
