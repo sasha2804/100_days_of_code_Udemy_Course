46651ed81714be669e9f21c5fbd60c2f4eb89fbe
sasha2804 <O.Korotushko@gmail.com>
2022-10-23 16:24:22 +0200

    123

--- 23_day/car_manager.py
+++ 23_day/car_manager.py
@@ -1,7 +1,32 @@
+from turtle import Turtle
+import random
+
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
 class CarManager:
-    pass
+    def __init__(self):
+        self.cars = []
+
+    def create_car(self):
+        rand_chance = random.randint(1,6)
+        if rand_chance == 1:
+        
+            new_car = Turtle('square')
+            new_car.shapesize(1, 2)
+            new_car.penup()
+            new_car.color(random.choice(COLORS))
+            new_car.goto(300, random.randint(-220,220))
+            self.cars.append(new_car)
+
+    def car_move(self):
+        for car in self.cars:
+            car.backward(25)
+    
+    def ret(self):
+        x = len(self.cars)
+        return(x)
+            
+
--- 23_day/main.py
+++ 23_day/main.py
@@ -1,5 +1,6 @@
 import time
 from turtle import Screen
+import turtle
 from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
@@ -7,8 +8,22 @@ from scoreboard import Scoreboard
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
+screen.listen()
+
+turtle = Player()
+car_manager = CarManager()
+
+screen.onkeypress(turtle.move_up, 'Up')
 
 game_is_on = True
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(0.35)
     screen.update()
+    car_manager.create_car()
+    car_manager.car_move()
+    x = car_manager.ret()
+    print(x)
+   
+
+
+screen.exitonclick()
