1e1370b5a0e85040f633bbb24d9afdffbf52be75
sasha2804 <O.Korotushko@gmail.com>
2022-10-30 15:27:29 +0100

    commit

---
+++ 24_day/score.py
@@ -0,0 +1,31 @@
+from ctypes import alignment
+from turtle import Turtle
+
+ALIGN = 'center'
+FONT = ('Arial',12,'normal')
+
+class Score(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.goto(0,280)        
+        self.penup()
+        self.hideturtle()
+        self.color('white')
+        self.scoreboard()
+
+    def scoreboard(self):
+        self.write(f'Score: {self.score}', align=ALIGN, font= FONT) 
+    
+    def score_print(self):
+        self.score += 1
+        self.clear()
+        self.scoreboard() 
+
+    def game_over(self):
+        self.goto(0,0)
+        self.write('GAME OVER', align=ALIGN, font= FONT)
+        
+
+                       
+        
