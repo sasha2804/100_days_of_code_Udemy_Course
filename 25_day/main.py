8ab67554345f7a57017f85b7f708f58e820cce35
sasha2804 <o.korotushko@gmail.com>
2022-11-02 14:40:25 +0100

    133

--- 25_day/main.py
+++ 25_day/main.py
@@ -1,5 +1,41 @@
 import pandas as pd
 
-data = pd.read_csv('25_day/weather_data.csv')
-print(data['temp'])
+data = pd.read_csv('25_day/2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv')
+df = data.groupby(['Primary Fur Color'])['Primary Fur Color'].count().to_dict()
+result_df = pd.DataFrame({'Color': list(df.keys()), 'Count': list(df.values())})
+print(result_df)
+result_df.to_csv('25_day/squirrel_colors.csv')
+
+
+
+# print(data.columns)
+# print(data['Primary Fur Color'].unique())
+# print((data['Primary Fur Color']=='Black').count())
+# color_list = list(data['Primary Fur Color'].unique())[1:]
+# print(color_list)
+# count_list = []
+# report = {'Fur color': [], 'Count': []}
+# data = pd.read_csv('25_day/weather_data.csv')
+# # print(data['temp'])
+# data_dict = data.to_dict()
+# print(data_dict)
+# temp_list = data['temp'].to_list()
+# # print(temp_list)
+# temp_av = sum(temp_list)/len(temp_list)
+# # print(temp_av)
+# av = data['temp'].mean()
+# # print(av)
+# max_temp = data['temp'].max()
+# # print(max_temp)
+# # print(data.condition)
+# print(data[data['temp'] == data['temp'].max()])
+# print(data)
+# # for i in range(len(data['temp'])):
+# #     data['temp'][i] = (data['temp'][i])*2 + 32    
+# # # print(dsta['temp'])
+# # print(data)
+# df_dict= {'students': ['Alex', 'Cook', 'Tom'], 'scores': [55,51,25]}
+# df = pd.DataFrame(df_dict)
+# print(df)
+# # df.to_csv('stundents.csv')
 
