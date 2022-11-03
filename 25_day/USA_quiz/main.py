339db84bf150d1f409990ba60f8c43b4bb98629e
sasha2804 <o.korotushko@gmail.com>
2022-11-03 16:29:11 +0100

    123

--- 25_day/USA_quiz/main.py
+++ 25_day/USA_quiz/main.py
@@ -16,8 +16,7 @@ turtle.shape(image)
 
 #read and prepare list of states
 states_list = pd.DataFrame(pd.read_csv('25_day/USA_quiz/50_states.csv'))
-states_list['state'] =  states_list['state'].map(lambda x: x.lower())
-print(states_list)
+set_states = set(states_list['state'])
 
 # create object for names writting on the map
 text = turtle.Turtle()
@@ -25,20 +24,31 @@ text.hideturtle()
 text.penup()
 
 states_guessed = set() # set of the guessed states
+states_to_learn = set() # set of states to be learned
 
 # loop to shop pop up window and write states names on the map
 while len(states_guessed) < len(states_list['state']):
-    answer_state = screen.textinput(title = f'{len(states_guessed)}/50 guessed states', prompt = 'Enter name of the state:')\
-        .lower()
-    if answer_state in set(states_list['state']) and answer_state not in states_guessed:
+    answer_state = screen.textinput(title = f'{len(states_guessed)}/50 guessed states', prompt = 'Enter name of the state:')
+    answer_state = str(answer_state).title()    
+
+    if answer_state in set_states and answer_state not in states_guessed:
         position = states_list[states_list['state'] == answer_state][['x', 'y']]
         text.goto(int(position['x']), int(position['y']))
         text.write(answer_state.title(),align = ALIGN, font = FONT)
-        states_guessed.add(answer_state)
-        print(answer_state)            
-    if answer_state.lower() == 'exit': # command to exit program
-        game_is_on = False
-   
+        states_guessed.add(answer_state)            
+    if answer_state.title() == 'Exit': # command to exit program
+        for i in set_states:
+            if i not in states_guessed:
+                states_to_learn.add(i)           
+        break
+
+# write states to learn to csv
+with open('25_day/USA_quiz/states_to_learn.csv', mode = 'w') as states_list:
+    for i in states_to_learn:
+        states_list.write(i+'\n')
+
+print('states to learn ', states_to_learn)
+
 
 
 
