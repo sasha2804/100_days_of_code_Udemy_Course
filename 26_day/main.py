1e04385b61bc0e69dbcea665404ebbb3b75f53ce
sasha2804 <o.korotushko@gmail.com>
2022-11-04 09:31:50 +0100

    123

---
+++ 26_day/main.py
@@ -0,0 +1,29 @@
+
+
+
+# lst = [x+2 for x in range(10) if x%2 == 0]
+
+# print(lst)
+
+# numbers = 'Angela'
+
+# new_numbers = [i for i in numbers]
+
+# print(new_numbers)
+
+# lst = [i*2 for i in range(1, 5) if i*2%4 == 0]
+
+# print(lst)
+
+# names = ['Alexander', 'James', 'Caroline', 'Tim']
+
+# new_names = [name.upper() for name in names if len(name) > 5]
+
+# print(new_names)
+
+# numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+
+# squared_numbers = [num**2 for num in numbers]
+
+# print(squared_numbers)
+
