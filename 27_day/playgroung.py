5968ee4af92599f8205aa7c529ee5b062e6ce169
sasha2804 <o.korotushko@gmail.com>
2022-11-11 16:59:42 +0100

    123

--- 27_day/playgroung.py
+++ 27_day/playgroung.py
@@ -2,11 +2,21 @@
 from ast import arg
 
 
-def test_func(*args):
-    print(sum(args))
-    for i in args:
-        print(i)
+# def test_func(*args):
+#     print(sum(args))
+#     for i in args:
+#         print(i)
     
 
 
-test_func(1,2,3,44,5)
+# test_func(1,2,3,44,5)
+
+
+def calc (**kwargs):
+    print(kwargs)
+
+
+
+
+
+calc(add=5, multiply=8)
