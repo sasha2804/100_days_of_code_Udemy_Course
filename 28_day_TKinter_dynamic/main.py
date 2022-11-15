5309c3912670aec39bd8240cdfff2eb51966c44c
sasha2804 <o.korotushko@gmail.com>
2022-11-15 15:06:00 +0100

    123

--- 28_day_TKinter_dynamic/main.py
+++ 28_day_TKinter_dynamic/main.py
@@ -1,8 +1,5 @@
 
 # ---------------------------- CONSTANTS ------------------------------- #
-from cgitb import text
-
-
 PINK = "#e2979c"
 RED = "#e7305b"
 GREEN = "#9bdeac"
@@ -12,13 +9,19 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 reps = 0
-
-
+timer = None
 # ---------------------------- TIMER RESET ------------------------------- # 
-
+def reset_timer():
+    global timer
+    global reps
+    window.after_cancel(timer)
+    label_top.config(text='Timer')
+    label_bottom.config(text='')
+    canvas.itemconfig(timer_text, text='00:00')
+    reps = 0
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def start_timer():
-    global reps
+    global reps    
     reps += 1
     work_reps = (1,3,5,7)
     pause_reps = (2,4,6)
@@ -33,17 +36,10 @@ def start_timer():
     elif reps in pause_reps:
         print('5 min reps',reps)         
         count_down(30)
-        label_top.config(text='Break', fg=PINK)
-      
-        
-           
-
-   
-
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
-
+        label_top.config(text='Break', fg=PINK)  
+ # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 def count_down(count):
-    global reps 
+    global timer
     minutes = count//60
     if minutes < 10:
         minutes = '0'+str(minutes)    
@@ -53,29 +49,18 @@ def count_down(count):
     timer_out = f'{minutes}:{seconds}'
     canvas.itemconfig(timer_text, text=timer_out)   
     if count > 0:    
-        window.after(100, count_down, count - 1)
+        timer = window.after(100, count_down, count - 1)
     else:
         start_timer()        
         if reps%2 == 0:
             print('mark and reps: ', reps)       
             check_mark = label_bottom.cget('text') + '✔'              
             label_bottom.config(text= check_mark)
-            
-        
-       
-
-
-
-# ---------------------------- UI SETUP ------------------------------- #
+ # ---------------------------- UI SETUP ------------------------------- #
 from tkinter import *
-
-
 window = Tk()
 window.title('Pomodoro by Korotushko')
 window.config(padx=100, pady=50, bg=YELLOW)
-
-
-
 
 # Canvas 
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
@@ -83,23 +68,20 @@ tomato_img = PhotoImage(file='28_day_TKinter_dynamic/tomato.png')
 canvas.create_image(100,112, image=tomato_img)
 timer_text = canvas.create_text(103,130, text='00:00', fill='white', font=(FONT_NAME, 30, 'bold'))
 canvas.grid(column=2 ,row=2)
-# count_down(5)
 
 # Labels
 label_top = Label(text='Timer', font=('Arial', 50, 'bold'), bg=YELLOW, fg=GREEN)
 label_top.grid(column=2, row=0)
 
-label_bottom = Label(text='', font=(FONT_NAME, 24, 'bold'), bg=YELLOW, fg=GREEN)
+label_bottom = Label(font=(FONT_NAME, 24, 'bold'), bg=YELLOW, fg=GREEN)
 label_bottom.grid(column=2, row=4)
 
 # Buttons
 button_start = Button(text='Start', font=FONT_NAME, highlightthickness=0, command=start_timer)
 button_start.grid(column=1, row=3)
 
-button_reset = Button(text='Reset', font=FONT_NAME, highlightthickness=0)
+button_reset = Button(text='Reset', font=FONT_NAME, highlightthickness=0, command=reset_timer)
 button_reset.grid(column=3, row=3)
-
-
 
 window.mainloop()
 
