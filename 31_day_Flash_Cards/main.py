0b634be4bcd8fd587bae5ff381d480b6b9d2dce3
sasha2804 <o.korotushko@gmail.com>
2022-11-28 17:43:49 +0100

    123

---
+++ 31_day_Flash_Cards/main.py
@@ -0,0 +1,69 @@
+
+#-------------------------------GETTING DATA FROM CSV-------------------------------
+from email.policy import default
+import pandas as pd
+import random
+data = pd.read_csv('31_day_Flash_Cards/data/french_words.csv')
+df = pd.DataFrame(data)
+data_dict = df.to_dict(orient='records') 
+# print(data_dict)
+
+
+
+type = True
+
+#-------------------------------NEXT CARD------------------------------
+def next_card():
+
+    language1 = 'French'
+    next_word = random.choice(data_dict)
+    canvas.itemconfig(card_title, text = language1)
+    canvas.itemconfig(card_word, text = next_word[language1])
+    
+
+
+
+#-------------------------------FLIP CARD-------------------------------
+def flip_card():
+    canvas.itemconfig(card_title, text='English')
+    # canvas.itemconfig(
+
+
+# canvas.itemconfig(canvas_image, image=new_image)
+
+    
+
+
+#-------------------------------SETTING USER INTERFACE-------------------------------
+BACKGROUND_COLOR = "#B1DDC6"
+
+from audioop import cross
+from tkinter import*
+from turtle import bgcolor, width
+window = Tk()
+window.title('Flash card by Korotushko')
+window.config(bg=BACKGROUND_COLOR, padx=50, pady=50)
+window.after(3000, flip_card)
+
+#word card canvas
+canvas = Canvas(width=800, height=526)
+canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
+img_front = PhotoImage(file='31_day_Flash_Cards/images/card_front.png')
+
+canvas_image = canvas.create_image(400,263, image=img_front)
+card_title = canvas.create_text(400,150, text='word1', fill='black', font=('Arial', 30, 'italic'))
+card_word = canvas.create_text(400,300, text='word2', fill='black', font=('Arial', 30, 'bold'))
+canvas.grid(column=0, row=0, columnspan=2)
+
+#buttons under the card configuration
+cross_image = PhotoImage(file='31_day_Flash_Cards/images/wrong.png')
+button_unknown = Button(image=cross_image, width=100, height=100, command=next_card)
+button_unknown.grid(column=0, row=1)
+
+checker_image = PhotoImage(file='31_day_Flash_Cards/images/right.png')
+button_unknown = Button(image=checker_image, width=100, height=100, command=next_card)
+button_unknown.grid(column=1, row=1)
+
+
+
+window.mainloop()
