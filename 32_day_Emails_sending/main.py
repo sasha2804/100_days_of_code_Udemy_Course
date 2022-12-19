4ea0c22a857c21c0f79fafdd9c9bd63b19d1382e
sasha2804 <o.korotushko@gmail.com>
2022-12-19 17:03:01 +0100

    123

--- 32_day_Emails_sending/main.py
+++ 32_day_Emails_sending/main.py
@@ -1,2 +1,24 @@
+from multiprocessing import connection
 import smtplib
+
+my_email = "o.korotushko@gmail.com"
+password = "ipzxnpatikiynqti"
+message = 'Hello, here is test message last message'
+
+
+# with smtplib.SMTP("smtp.gmail.com") as connection:
+#     connection.starttls()
+#     connection.login(user=my_email, password=password)
+#     connection.sendmail(from_addr=my_email, to_addrs="o.korotushko@yahoo.com", msg=f"Subject:Hello\n\n{message}")
+#     # connection.close()
+
+
+connection =  smtplib.SMTP("smtp.gmail.com")
+connection.starttls()
+connection.login(user=my_email, password=password)
+connection.sendmail(from_addr=my_email, to_addrs="o.korotushko@yahoo.com", msg=f"Subject:Hello\n\n{message}")
+connection.close()
+
+
+
 
