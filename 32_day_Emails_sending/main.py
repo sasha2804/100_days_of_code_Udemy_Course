64707bc66967925a4298bccc090cbd3923276ec2
sasha2804 <o.korotushko@gmail.com>
2022-12-20 14:43:55 +0100

    123

--- 32_day_Emails_sending/main.py
+++ 32_day_Emails_sending/main.py
@@ -1,23 +1,48 @@
-from multiprocessing import connection
-import smtplib
+# from multiprocessing import connection
+# import smtplib
 
-my_email = "o.korotushko@gmail.com"
-password = "ipzxnpatikiynqti"
-message = 'Hello, here is test message last message'
-
-
-# with smtplib.SMTP("smtp.gmail.com") as connection:
-#     connection.starttls()
-#     connection.login(user=my_email, password=password)
-#     connection.sendmail(from_addr=my_email, to_addrs="o.korotushko@yahoo.com", msg=f"Subject:Hello\n\n{message}")
-#     # connection.close()
+# my_email = "o.korotushko@gmail.com"
+# password = "ipzxnpatikiynqti"
+# message = 'Hello, here is test message last message'
 
 
-connection =  smtplib.SMTP("smtp.gmail.com")
-connection.starttls()
-connection.login(user=my_email, password=password)
-connection.sendmail(from_addr=my_email, to_addrs="o.korotushko@yahoo.com", msg=f"Subject:Hello\n\n{message}")
-connection.close()
+# # with smtplib.SMTP("smtp.gmail.com") as connection:
+# #     connection.starttls()
+# #     connection.login(user=my_email, password=password)
+# #     connection.sendmail(from_addr=my_email, to_addrs="o.korotushko@yahoo.com", msg=f"Subject:Hello\n\n{message}")
+# #     # connection.close()
+
+
+# connection =  smtplib.SMTP("smtp.gmail.com")
+# connection.starttls()
+# connection.login(user=my_email, password=password)
+# connection.sendmail(from_addr=my_email, to_addrs="o.korotushko@yahoo.com", msg=f"Subject:Hello\n\n{message}")
+# connection.close()
+
+
+import datetime as dt
+import pandas as pd
+import random as rd
+
+# now =  dt.datetime.now()
+# day = now.weekday()
+# print(now.year, now.month, now.day, day)
+
+
+with open('32_day_Emails_sending/quotes.txt') as quotes:
+    list = quotes.readlines()
+
+
+print(list[0])
+
+print('random: ',rd.choice(list))
+
+
+
+
+
+
+
 
 
 
