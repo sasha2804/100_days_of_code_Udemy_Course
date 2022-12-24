ba8adb5d4dfd3a176344906388f8ee725c56b056
sasha2804 <o.korotushko@gmail.com>
2022-12-24 19:17:56 +0200

    123

--- 32_day_Emails_sending/Challenge/main.py
+++ 32_day_Emails_sending/Challenge/main.py
@@ -1,13 +1,20 @@
 ##################### Extra Hard Starting Project ######################
 
-# 1. Update the birthdays.csv
-
+# 1. Update the birthdays.csv DONE
 # 2. Check if today matches a birthday in the birthdays.csv
-
 # 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
-
 # 4. Send the letter generated in step 3 to that person's email address.
 
 
+#TODO 1 read csv -> make DataFrame
+#TODO 2 make search in date column DataFrame
+#TODO 3 import text from random letter
+#TODO 4 replace PLACE HOLDER in the letter with data from DataFrame
 
+
+import pandas as pd
+
+
+
+people_data = pd.
 
