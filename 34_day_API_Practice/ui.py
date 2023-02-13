7c61f9a6e314b0d72b885bea7f8f36e3e2f3215f
sasha2804 <o.korotushko@gmail.com>
2023-02-13 13:56:29 +0100

    commit 13.02.2023

--- 34_day_API_Practice/ui.py
+++ 34_day_API_Practice/ui.py
@@ -14,21 +14,25 @@ class QuizInterface:
         # LABELS
         self.label_score = Label(text='Score:', textvariable=TEXT_VAR, bg=THEME_COLOR,font=('Arial', 14, 'bold'),foreground='white')
         # label_score.config(foreground='white')
-        self.label_score.grid(column=1,row=1,pady = 20, padx= 20)
+        # self.label_score.grid(column=1,row=1,pady = 20, padx= 20)
+        self.label_score.grid(column=1,row=1, pady=20)
 
         # CANVAS
         self.canvas = Canvas(width=300, height=250,highlightthickness=0, bg='white')
-        self.timer_text = self.canvas.create_text(103,130, text='TEST', fill='white', font=('Arial', 20, 'italic'))
-        self.canvas.grid(column=0 ,row=2, padx = 20, columnspan = 2)
+        self.question_text = self.canvas.create_text(150,125, text='SOME TEXT IS HERE SOME TEXT IS HERE', fill='black', font=('Arial', 20, 'italic'), width=300)
+        # self.canvas.grid(column=0 ,row=2, padx = 20, columnspan = 2)
+        self.canvas.grid(column=0 ,row=2, columnspan = 2)
 
         #BUTTONS
         self.true_img = PhotoImage(file='34_day_API_Practice/images/true.png')    
         self.button_start = Button(image=self.true_img, font=FONT_NAME, highlightthickness=0, pady = 0, padx= 0)
-        self.button_start.grid(column=0, row=3, pady = 20, padx= 20)
+        # self.button_start.grid(column=0, row=3, pady = 20, padx= 20)
+        self.button_start.grid(column=0, row=3, pady=20)
 
         self.false_img = PhotoImage(file='34_day_API_Practice/images/false.png')    
         self.button_false = Button(image=self.false_img, font=FONT_NAME, highlightthickness=0, pady = 0, padx= 0)
-        self.button_false.grid(column=1, row=3, pady = 20, padx = 20)
+        # self.button_false.grid(column=1, row=3, pady = 20, padx = 20)
+        self.button_false.grid(column=1, row=3, pady=20)
 
         self.window.mainloop()
 
