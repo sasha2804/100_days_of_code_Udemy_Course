870ac63f0c1f509d9046adff7756e0b3add9e090
sasha2804 <o.korotushko@gmail.com>
2023-02-20 21:08:16 +0200

    1212

--- 35_day_API_Authentification_Keys/main.py
+++ 35_day_API_Authentification_Keys/main.py
@@ -8,12 +8,12 @@ api_key = '3bef3c17fb5292d10c2768155ff15bcd'
 
 parameters = {
     'lat' : lat,
-    'long' : lon, 
-    'appid' : api_key
+    'lon' : lon, 
+    'appid' : api_key,
 }
 
-response = requests.get(url= 'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}', params=parameters)
-# response.raise_for_status()
+response = requests.get(url= f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}', params=parameters)
+response.raise_for_status()
 current_weather = response.json()
 
 print(current_weather)
