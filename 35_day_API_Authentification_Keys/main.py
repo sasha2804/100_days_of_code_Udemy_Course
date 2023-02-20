93a4d4c23ae0d4cef8126fb1ead856d6e2ed1e28
sasha2804 <o.korotushko@gmail.com>
2023-02-20 21:07:25 +0200

    20.02.2023 21:07 commit

---
+++ 35_day_API_Authentification_Keys/main.py
@@ -0,0 +1,22 @@
+import requests
+
+url_weather = 'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}'
+
+lat = 49.252750
+lon = 8.879400
+api_key = '3bef3c17fb5292d10c2768155ff15bcd'
+
+parameters = {
+    'lat' : lat,
+    'long' : lon, 
+    'appid' : api_key
+}
+
+response = requests.get(url= 'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}', params=parameters)
+# response.raise_for_status()
+current_weather = response.json()
+
+print(current_weather)
+
+
+
