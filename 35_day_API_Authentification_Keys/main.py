ad61e990b0ef87c892f3df37100ede0ca63d7814
sasha2804 <o.korotushko@gmail.com>
2023-02-21 15:13:05 +0100

    commit 21.02.2023 15:12

--- 35_day_API_Authentification_Keys/main.py
+++ 35_day_API_Authentification_Keys/main.py
@@ -1,18 +1,21 @@
+from click import ParamType
 import requests
 
-url_weather = 'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}'
+url_weather = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}"
+
 
 lat = 49.252750
 lon = 8.879400
-api_key = '3bef3c17fb5292d10c2768155ff15bcd'
+api_key = "3bef3c17fb5292d10c2768155ff15bcd"
 
 parameters = {
-    'lat' : lat,
-    'lon' : lon, 
-    'appid' : api_key,
+    "lat" : lat,
+    "lon" : lon, 
+    "appid" : api_key,
+    "exclude": "current,minutely,daily"
 }
 
-response = requests.get(url= f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}', params=parameters)
+response = requests.get(url= f'https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={parameters["exclude"]}&appid={api_key}', params=parameters)
 response.raise_for_status()
 current_weather = response.json()
 
