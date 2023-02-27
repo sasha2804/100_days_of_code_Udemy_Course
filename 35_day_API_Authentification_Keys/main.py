8b07528c17d90407ed3a02491f7144472d391716
sasha2804 <o.korotushko@gmail.com>
2023-02-27 13:12:56 +0100

    27.02.2023 commit

--- 35_day_API_Authentification_Keys/main.py
+++ 35_day_API_Authentification_Keys/main.py
@@ -3,6 +3,35 @@ import requests
 
 url_weather = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}"
 
+twilio_code = "0yFJM4r-cziFI9FS9sFftUY-7lH0H1JrRUnj6ePf" 
+'''
+If you lose your phone, or don't have access to your verification device, this code is your failsafe to access your account.
+'''
+twilio_trial_number = "+12766249742"
+
+
+# import os
+from twilio.rest import Client
+
+
+# Find your Account SID and Auth Token at twilio.com/console
+# and set the environment variables. See http://twil.io/secure
+account_sid = 'AC1e639d993da7c887ff46e248d39e8d61'
+auth_token = '49728e3b08f434fc84d46f5ded905473'
+client = Client(account_sid, auth_token)
+
+message = client.messages \
+                .create(
+                     body="Join Earth's mightiest heroes. Like Kevin Bacon.",
+                     from_='+12766249742',
+                     to='+380978308027'
+                 )
+
+print(message.sid)
+
+
+
+
 
 lat = 49.252750
 lon = 8.879400
---
+++ Test/test.py
@@ -0,0 +1,6 @@
+x = '2'
+y = '3'
+
+for i in x:
+    print(i)
+    
