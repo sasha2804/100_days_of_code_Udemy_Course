85d955f144c63da7542fe35c35a7bcee06bbb513
sasha2804 <o.korotushko@gmail.com>
2023-03-17 11:22:04 +0100

    123

--- Leetcode/26_remove_duplicates_from_sorted_array.py
+++ Leetcode/26_remove_duplicates_from_sorted_array.py
@@ -1,29 +1,59 @@
 
 
 
-class Solution:
-    # def removeDuplicates(self, nums: List[int]) -> int:
-    def removeDuplicates(self, nums):
+# class Solution:
+#     # def removeDuplicates(self, nums: List[int]) -> int:
+#     def removeDuplicates(self, nums):
 
-        ret = []
+#         ret = []
 
-        for i in nums:
-            if i not in ret:
-                ret.append(i)
-            else:
-                ret.append("_")
+#         for i in nums:
+#             if i not in ret:
+#                 ret.append(i)
+#             else:
+#                 ret.append("_")
         
-        nums = ret
-        k = len(nums)
-        # return  'nums=nums)
+#         nums = ret
+#         k = len(nums)
+#         # return  'nums=nums)
 
 
-test = Solution()
+# test = Solution()
 
 
 
-lst = [1,1,2]
+# lst = [1,1,2]
 
 
-print(test.removeDuplicates(lst))
+# print(test.removeDuplicates(lst))
+
+
+# s = 'x'
+# s_temp = ''
+
+# # count = 0
+
+# # for i in s:
+# #     count += 1
+# #     s_temp += i
+
+# # print(count)
+# # print('s_temp: ', s_temp)
+# # print('len s_temp: ', len(s_temp))
+
+
+# # for i in s:
+# #     if i in s_temp:
+# #         print('ok')
+
+
+# print(s*2)
+
+
+s = 'afdsas'
+
+
+print(s[s.index('d'):])
+
+
 
--- Leetcode/3_Longest_Substring.py
+++ Leetcode/3_Longest_Substring.py
@@ -2,29 +2,92 @@
 
 # s = "bbbbbbb"
 
-s='pwwkew'
+# s='pwwkew'
+
+# s = "au"
+
+# s = "aab"
+
+s = "dvdfg"
+
+# s = "abcabcbb"
+
+# s = 'vdvfg'
+
+# s = '   '
+
+s="pwwkew"
+
+# s="aaaaaaaaaaa"
 
 max_len = 0
 count = 0
-s_temp = ''
+s_temp = ""
+count1 = 0
 
+if len(s) == 1:
+    s = s*2
 
 for i in s:
+    count1 += 1          
     if i not in s_temp:
         s_temp += i
         count += 1
-        print(s_temp)
-        # print(i)
-        # print('1')
+        print(i)
+        if count1 == len(s):
+            max_len = count
     else:
-        s_temp = s_temp[-1]
-        print(s_temp)
+        s_temp = s_temp[s_temp.index(i):]
+        print('s_temp: ', s_temp)
+        # s_temp += i
+        print('s_temp: ', s_temp)
+        
         if count > max_len:
             max_len = count
-            count = 0
-            # print('end')
+
+print(max_len)
+
+
+
+# for j in range(0, len(s)):
+#     count1 += 1
+#     for i in s:          
+#         if i not in s_temp:
+#             s_temp += i
+#             count += 1
+#             # print(i)
+#             if count1 == len(s):
+#                 max_len = count
+#         else:
+#             s_temp = i
+#             if count > max_len:
+#                 max_len = count
+#                 count = 1
+    
+  
 
 
 print('max len: ', max_len)
+print('count1: ', count1)
+
+
+# for i in s:
+#     count1 += 1
+   
+#     if chr(ord(i)) not in s_temp:
+#         s_temp += chr(ord(i))
+#         count += 1
+#     else:
+#         s_temp = chr(ord(i))
+#         if count > max_len:
+#             max_len = count
+#             count = 1
+
+
+
+
+# print('max len: ', max_len)
+# print('count1: ', count1)
+
 
 
