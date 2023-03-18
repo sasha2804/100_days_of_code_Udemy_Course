c80de23255bd841a23934557e14bb034f2f1765c
sasha2804 <o.korotushko@gmail.com>
2023-03-18 16:49:22 +0200

    123

--- Leetcode/3_Longest_Substring.py
+++ Leetcode/3_Longest_Substring.py
@@ -1,115 +1,51 @@
-# s = "abcabcbb"
-
-# s = "bbbbbbb"
-
+s = "abcabcbb"
+# s = "ua"
 # s='pwwkew'
+# s = "dvdfg"
+# s = ' '
+# s = "pwwkew"
+# s = 'abba'
+# s = '    '
+s = "jbpnbwwd"
+# s = ' '
 
-# s = "au"
 
-# s = "aab"
-
-s = "dvdfg"
-
-# s = "abcabcbb"
-
-# s = 'vdvfg'
-
-# s = '   '
-
-# s="pwwkew"
-
-# s="aaaaaaaaaaa"
-
+ls_temp = []
+s_temp = ''
 max_len = 0
-count = 0
-s_temp = ""
-count1 = 0
 
-# s="pwwkew"
-s="abctgabcd"
-
-s = "abba"
-# s = 'cdd'
-
-# s = "auda"
 
 if len(s) == 1:
-    s = s*2
+    s *= 2
 
 for i in s:
-    count1 += 1          
-    if i not in s_temp:
-        s_temp += i
-        print('s_temp: ', s_temp)
+    for j in s:
+        if j not in s_temp:
+            s_temp += j            
+        else:
+            ls_temp.append(s_temp)            
+            if len(s_temp) > max_len:
+                max_len = len(s_temp)
+            s_temp = ''
+            break
+    s = s[1:]
 
-        count += 1
-        # print(i)
-        if count1 == len(s):
-            max_len = count
-            print('max len: ', max_len)
-    else:        
-        print('s_temp in!!!!: ', s_temp)
-        s_temp = s_temp[s_temp.index(i):]
-        print('s_temp in!!!!: ', s_temp)
-
-        
-        # print('s_temp: ', s_temp)
-        s_temp += i
-        l = len(s_temp)
-        
-        
-        # print('length: ',l)
-        # count -= 1
-        
-        if max_len < count:
-            max_len = count
-            # print('max len: ', max_len)
-            count = 0
-        
-
+print(ls_temp)
 print(max_len)
 
 
+# class Solution:
+#     def lengthOfLongestSubstring(self, s: str) -> int:
+#         char_set = set()
+#         max_len, start = 0, 0
+#         for i, c in enumerate(s):
+#             while c in char_set:
+#                 char_set.remove(s[start])
+#                 start += 1
+#             char_set.add(c)
+#             max_len = max(max_len, i - start + 1)
+#         return max_len
 
-# for j in range(0, len(s)):
-#     count1 += 1
-#     for i in s:          
-#         if i not in s_temp:
-#             s_temp += i
-#             count += 1
-#             # print(i)
-#             if count1 == len(s):
-#                 max_len = count
-#         else:
-#             s_temp = i
-#             if count > max_len:
-#                 max_len = count
-#                 count = 1
-    
-  
-
-
-print('max len: ', max_len)
-print('count1: ', count1)
-
-
-# for i in s:
-#     count1 += 1
-   
-#     if chr(ord(i)) not in s_temp:
-#         s_temp += chr(ord(i))
-#         count += 1
-#     else:
-#         s_temp = chr(ord(i))
-#         if count > max_len:
-#             max_len = count
-#             count = 1
-
-
-
-
-# print('max len: ', max_len)
-# print('count1: ', count1)
 
 
 
--- Leetcode/test.py
+++ Leetcode/test.py
@@ -1,172 +1,200 @@
-# x = '2'
-# y = '3'
+# # x = '2'
+# # y = '3'
 
-# for i in x:
-#     print(i)
+# # for i in x:
+# #     print(i)
     
 
 
-# class Solution:
-#     def twoSum(self, nums, target):
-#         for i in range(0, len(nums)):
-#             for j in range(0, len(nums)):
-#                 if i != j:
-#                     s = nums[i] + nums[j]
-#                     if s == target:
-#                         return([i, j])               
+# # class Solution:
+# #     def twoSum(self, nums, target):
+# #         for i in range(0, len(nums)):
+# #             for j in range(0, len(nums)):
+# #                 if i != j:
+# #                     s = nums[i] + nums[j]
+# #                     if s == target:
+# #                         return([i, j])               
   
 
 
-# test = Solution()
+# # test = Solution()
 
-# nums = [3,2,4]
-# target = 6
+# # nums = [3,2,4]
+# # target = 6
 
-# nums = [2,7,11,15]
-# target = 9
+# # nums = [2,7,11,15]
+# # target = 9
 
-# test.twoSum(nums, target)
+# # test.twoSum(nums, target)
 
-# print(test.twoSum(nums, target))
+# # print(test.twoSum(nums, target))
 
 
-'''
-Input: nums = [2,7,11,15], target = 9
-Output: [0,1]
-Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
-'''
+# '''
+# Input: nums = [2,7,11,15], target = 9
+# Output: [0,1]
+# Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+# '''
 
-# nums = [2,7,11,15]
-# target = 9
+# # nums = [2,7,11,15]
+# # target = 9
 
-# for i in range(0, len(nums)-1):
-#     for j in range(0, len(nums)-1):
-#         if i != j:
-#             s = nums[i] + nums[j]
-#             if s == target:
-#                 print([i, j])               
-#     break
+# # for i in range(0, len(nums)-1):
+# #     for j in range(0, len(nums)-1):
+# #         if i != j:
+# #             s = nums[i] + nums[j]
+# #             if s == target:
+# #                 print([i, j])               
+# #     break
+
+# # class Solution:
+# #     def twoSum(self, nums, target):
+# #         for i in range(0, len(nums)-1):
+# #             for j in range(0, len(nums)-1):
+# #                 if i != j:
+# #                     s = nums[i] + nums[j]
+# #                     if s == target:
+# #                         return([i, j])       
+
+
+
+# # class Solution:
+
+# #     def isPalindrome(self, x):
+# #         rev_x = (str(x)[::-1])        
+# #         if rev_x == str(x):
+# #             return True
+# #         return False
+
+# #         # print(rev_x)
+
+
+# # is_palindrome = Solution()
+
+# # print(is_palindrome.isPalindrome(121))
+
+
+# from tkinter import S
+
+
+# # s = "MCMXCIV"
+# # s = "MCMXCIV"
+# s = "LVIII"
+# # s = "III"
+# # s = "MMMIIX"
+
+# '''
+# https://www.rapidtables.com/convert/number/roman-numerals-converter.html?x1=MXXXXVVX&x2=
+# '''
+
+# dict = {
+#     'I': 1,
+#     'V': 5,
+#     'X': 10,
+#     'L': 50,
+#     'C': 100,
+#     'D': 500,
+#     'M': 1000
+# }
+
+
 
 # class Solution:
-#     def twoSum(self, nums, target):
-#         for i in range(0, len(nums)-1):
-#             for j in range(0, len(nums)-1):
-#                 if i != j:
-#                     s = nums[i] + nums[j]
-#                     if s == target:
-#                         return([i, j])       
-
-
-
-# class Solution:
-
-#     def isPalindrome(self, x):
-#         rev_x = (str(x)[::-1])        
-#         if rev_x == str(x):
-#             return True
-#         return False
-
-#         # print(rev_x)
-
-
-# is_palindrome = Solution()
-
-# print(is_palindrome.isPalindrome(121))
-
-
-from tkinter import S
-
-
-# s = "MCMXCIV"
-# s = "MCMXCIV"
-s = "LVIII"
-# s = "III"
-# s = "MMMIIX"
-
-'''
-https://www.rapidtables.com/convert/number/roman-numerals-converter.html?x1=MXXXXVVX&x2=
-'''
-
-dict = {
-    'I': 1,
-    'V': 5,
-    'X': 10,
-    'L': 50,
-    'C': 100,
-    'D': 500,
-    'M': 1000
-}
-
-
-
-class Solution:
-    def romanToInt(self, roman_num):
-        trig = 0
-        lst = []
-        lst_final = []
-        result = 0
+#     def romanToInt(self, roman_num):
+#         trig = 0
+#         lst = []
+#         lst_final = []
+#         result = 0
         
-        dict = {
-            'I': 1,
-            'V': 5,
-            'X': 10,
-            'L': 50,
-            'C': 100,
-            'D': 500,
-            'M': 1000
-        }
-        for i in range(0, len(roman_num)):            
-            x = dict[roman_num[i]]
-            lst.append(x)              
-        while len(lst) != 0:
-            if len(lst) == 1 and trig == 0:
-                lst_final.append(list[0])
-                lst.pop(0)
-                break
-            if len(lst) == 1 and trig == 1:
-                if lst[0] > lst_final[-1]/2:
-                    lst_final.clear()
-                else:
-                    lst_final[-1] += lst[0]
-                    lst.pop(0)
-                break
-            if lst[0] > lst[1]:
-                lst_final.append(lst[0])
-                lst.pop(0)
-            elif lst[0] < lst[1]:
+#         dict = {
+#             'I': 1,
+#             'V': 5,
+#             'X': 10,
+#             'L': 50,
+#             'C': 100,
+#             'D': 500,
+#             'M': 1000
+#         }
+#         for i in range(0, len(roman_num)):            
+#             x = dict[roman_num[i]]
+#             lst.append(x)              
+#         while len(lst) != 0:
+#             if len(lst) == 1 and trig == 0:
+#                 lst_final.append(list[0])
+#                 lst.pop(0)
+#                 break
+#             if len(lst) == 1 and trig == 1:
+#                 if lst[0] > lst_final[-1]/2:
+#                     lst_final.clear()
+#                 else:
+#                     lst_final[-1] += lst[0]
+#                     lst.pop(0)
+#                 break
+#             if lst[0] > lst[1]:
+#                 lst_final.append(lst[0])
+#                 lst.pop(0)
+#             elif lst[0] < lst[1]:
                 
-                if lst[1]/2 == lst[0]:
-                    lst_final.clear()
-                    break
+#                 if lst[1]/2 == lst[0]:
+#                     lst_final.clear()
+#                     break
                 
-                lst_final.append(lst[1] - lst[0])
-                lst.pop(0)
-                lst.pop(0)
-            elif lst[0] == lst[1]:
-                lst_final.append(lst[0] + lst[1])
-                lst.pop(0)
-                lst.pop(0)
-                trig = 1
-        # print(type(sum(lst_final)))
-        # 
-        print(type(lst_final[0]))
+#                 lst_final.append(lst[1] - lst[0])
+#                 lst.pop(0)
+#                 lst.pop(0)
+#             elif lst[0] == lst[1]:
+#                 lst_final.append(lst[0] + lst[1])
+#                 lst.pop(0)
+#                 lst.pop(0)
+#                 trig = 1
+#         # print(type(sum(lst_final)))
+#         # 
+#         print(type(lst_final[0]))
 
-        print(lst_final)
-        print(sum(lst_final))        
+#         print(lst_final)
+#         print(sum(lst_final))        
         
-        if len(lst_final) != 0:        
-            for j in (lst_final):
-                result += j
-                print(type(result))          
-            return(result)
-        return None
+#         if len(lst_final) != 0:        
+#             for j in (lst_final):
+#                 result += j
+#                 print(type(result))          
+#             return(result)
+#         return None
 
 
     
 
 
-text_class = Solution()
-# text_class(s)
+# text_class = Solution()
+# # text_class(s)
 
-print(text_class.romanToInt(s))
+# print(text_class.romanToInt(s))
+
+
+
+s = "abcabcbb"
+
+print(s[1:])
+
+# strt = ''
+
+# for i in range(0, len(s)):
+
+
+#     for j in range(i, len(s)):
+#         strt += s[j]
+
+
+#     print(strt)
+#     strt = ''
+
+
+# for k, m in enumerate(s):
+#     print(k)
+#     print(m)
+
+
+# # print(enumerate(s))
+
+
 
