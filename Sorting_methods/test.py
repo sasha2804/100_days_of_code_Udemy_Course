8a2be4a86a9662d2ce622ae982ed4143ac8986a0
sasha2804 <o.korotushko@gmail.com>
2023-04-12 18:07:50 +0200

    123

--- Sorting_methods/test.py
+++ Sorting_methods/test.py
@@ -1,8 +1,25 @@
 
 
-arr = [5,4,2,1]
+# arr = [5,4,2,1]
 
 
-arr[0] = 3
+# arr[0] = 3
 
-print(arr)
+# print(arr)
+
+
+
+def rec(x):
+
+    if x != 1:
+        print('before: ', x)
+        rec(x-1) 
+        
+        print('after: ', x)
+  
+
+
+rec(5)
+
+        
+    
