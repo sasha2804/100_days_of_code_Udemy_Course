3c2f50175fd13a9ab46e1fbfebe411b0ef36c11f
sasha2804 <o.korotushko@gmail.com>
2023-06-19 19:53:54 +0200

    123

--- 36_Stock_Trading/main.py
+++ 36_Stock_Trading/main.py
@@ -60,18 +60,29 @@ print(f'difference percentage: {perc_print}%')
 
 #TODO 5. - If TODO4 percentage is greater than 5 then print("Get News").
 
-
     ## STEP 2: https://newsapi.org/ 
+    # API key 44268d9d294249ed9ed3044dbfd7e7c0
+
     # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
 
 #TODO 6. - Instead of printing ("Get News"), use the News API to get articles related to the COMPANY_NAME.
 
-#TODO 7. - Use Python slice operator to create a list that contains the first 3 articles. Hint: https://stackoverflow.com/questions/509211/understanding-slice-notation
+# STOCK_NAME = "TSLA"
+# PERIOD = "TIME_SERIES_DAILY_ADJUSTED"
+KEY1 = '44268d9d294249ed9ed3044dbfd7e7c0'
+url = f'https://newsapi.org/v2/everything?q=tesla&from=2023-05-19&sortBy=publishedAt&apiKey={KEY1}'
+'https://newsapi.org/v2/everything?q=tesla&from=2023-05-19&sortBy=publishedAt&apiKey=44268d9d294249ed9ed3044dbfd7e7c0'
+response_news = requests.get(url)
+response_news.raise_for_status()
+data_news = response_news.json()
 
+#TODO 7. - Use Python slice operator to create a list that contains the first 3 articles. Hint: https://stackoverflow.com/questions/509211/understanding-slice-notation
     ## STEP 3: Use twilio.com/docs/sms/quickstart/python
     #to send a separate message with each article's title and description to your phone number. 
 
 #TODO 8. - Create a new list of the first 3 article's headline and description using list comprehension.
+headlines = [data_news["articles"][x]['title'] for x in range(4)]
+print(headlines)
 
 #TODO 9. - Send each article as a separate message via Twilio. 
 
--- test.py
+++ test.py
@@ -1,30 +1,33 @@
-from datetime import datetime, timedelta
+# from datetime import datetime, timedelta
 
-import datetime as dt
+# import datetime as dt
 
-current_date = dt.datetime.now()
-cur_date = current_date.date()
-current_week_day = current_date.weekday()
+# current_date = dt.datetime.now()
+# cur_date = current_date.date()
+# current_week_day = current_date.weekday()
 
-if current_week_day != 5 and current_week_day != 6:
-    diff = 1
-    if current_week_day == 0:
-        diff = 3
-
-
-
-previous_date = current_date - timedelta(days=diff)
-
-print(type(current_date))
-
-print(type(timedelta(days=diff)))
+# if current_week_day != 5 and current_week_day != 6:
+#     diff = 1
+#     if current_week_day == 0:
+#         diff = 3
 
 
 
+# previous_date = current_date - timedelta(days=diff)
 
-print(previous_date)
+# print(type(current_date))
+
+# print(type(timedelta(days=diff)))
 
 
+
+
+# print(previous_date)
+
+
+newlist = [x for x in range(10)]
+
+print(newlist)
 
 
 
