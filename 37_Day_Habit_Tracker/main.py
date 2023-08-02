3b678c86be05af9fc43d2d37237329a0951463f1
sasha2804 <o.korotushko@gmail.com>
2023-08-02 14:12:19 +0200

    02082023

--- 37_Day_Habit_Tracker/main.py
+++ 37_Day_Habit_Tracker/main.py
@@ -1,9 +1,11 @@
 
 from urllib import response
+from urllib.request import URLopener
 import requests
 
 USERNAME = "oleksandr1987"
 TOKEN = "123456token!oleks"
+GRAPH_ID = "graph1"
 
 pixela_endpoint = 'https://pixe.la/v1/users'
 
@@ -30,11 +32,38 @@ user_params = {
 #     "type": "int",
 #     "color": "ajisai"
 # }
-# headers = {
-#     "X-USER-TOKEN": TOKEN
-# }
-
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
 # response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
 # print(response.text)
 
 
+
+
+
+
+
+
+write_params = {
+    "date":"20230725",
+    "quantity":"2"     
+}
+
+write_point =  f"https://pixe.la/v1/users/{USERNAME}/graphs/{GRAPH_ID}"
+
+write_resp = requests.post(url=write_point,json=write_params, headers=headers)
+print(write_resp.text)
+
+
+
+
+
+# $ curl -X POST https://pixe.la/v1/users -d '{"token":"thisissecret", "username":"a-know", "agreeTermsOfService":"yes", "notMinor":"yes", "thanksCode":"ThisIsThanksCode"}'
+# {"message":"Success. Let's visit https://pixe.la/@a-know , it is your profile page!","isSuccess":true}
+
+
+
+'''
+https://pixe.la/v1/users/oleksandr1987/graphs/graph1
+'''
