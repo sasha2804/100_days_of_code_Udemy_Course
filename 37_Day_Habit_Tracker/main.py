606aaf5a238b09a483a0c32fb26c72471bc7051e
sasha2804 <o.korotushko@gmail.com>
2023-08-03 08:08:39 +0200

    123

--- 37_Day_Habit_Tracker/main.py
+++ 37_Day_Habit_Tracker/main.py
@@ -44,15 +44,17 @@ headers = {
 
 
 
+# today = dt.date.today()
+# date_str = today.strftime("%Y%m%d")
 
 
 
 # write_params = {
-#     "date": "230705",
+#     "date": date_str,
 #     "quantity":"2"     
 # }
 
-# write_point =  f"https://pixe.la/v1/users/{USERNAME}/graphs/{GRAPH_ID}"
+write_point =  f"https://pixe.la/v1/users/{USERNAME}/graphs/{GRAPH_ID}"
 
 # write_resp = requests.post(url=write_point,json=write_params, headers=headers)
 # print(write_resp.text)
@@ -64,6 +66,28 @@ headers = {
 # $ curl -X POST https://pixe.la/v1/users -d '{"token":"thisissecret", "username":"a-know", "agreeTermsOfService":"yes", "notMinor":"yes", "thanksCode":"ThisIsThanksCode"}'
 # {"message":"Success. Let's visit https://pixe.la/@a-know , it is your profile page!","isSuccess":true}
 
+# date = "20230803"
+
+# update_params = {    
+#     "quantity":"25"  
+#     }
+
+
+# update_point =  f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{date}"
+
+# update_resp = requests.put(url=update_point,json=update_params, headers=headers)
+# print(update_resp.text)
+
+date = "20230803"
+
+del_point =  f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{date}"
+
+del_resp = requests.delete(url=del_point, headers=headers)
+print(del_resp.text)
+
+
+
+
 
 
 '''
@@ -71,8 +95,3 @@ https://pixe.la/v1/users/oleksandr1987/graphs/graph1
 '''
 
 
-today = dt.date.today()
-print(today)
-
-date_str = today.strftime("%Y%m%d")
-print(date_str)
