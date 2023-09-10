69ba70b82c8658c04d689bae83de2281096cf7d4
sasha2804 <o.korotushko@gmail.com>
2023-09-10 20:43:05 +0200

    123

--- 45_day_Parsing_HTML_Making_Soup/1_resources/main.py
+++ 45_day_Parsing_HTML_Making_Soup/1_resources/main.py
@@ -1,4 +1,24 @@
+from bs4 import BeautifulSoup
+# import lxml
 
 
-with open("website.html") as my_file:
-    print(my_file)
+with open('45_day_Parsing_HTML_Making_Soup/1_resources/website.html',encoding="utf8") as my_file:
+    content = my_file.read()
+
+soup = BeautifulSoup(content, "html.parser")
+
+# print(soup.title.string)
+
+anchor_tags = soup.find_all(name='a')
+# print(anchor_tags)
+
+# for tag in anchor_tags:
+#     # print(tag.getText())
+#     print(tag.get("href"))
+
+    
+
+heading = soup.find_all(name='h1', id='name')
+
+
+print(heading)
