966254c89decb5365b6ccce0c86c5942c26b660f
sasha2804 <o.korotushko@gmail.com>
2023-10-08 19:58:32 +0200

    123

--- 45_day_Parsing_HTML_Making_Soup/2_project/main.py
+++ 45_day_Parsing_HTML_Making_Soup/2_project/main.py
@@ -11,13 +11,31 @@ yc_web_page = response.text
 # print(yc_web_page)
 
 soup = BeautifulSoup(yc_web_page, 'html.parser')
-
 # print(soup.prettify())
-
 # print(soup.li)
 
 movies_list = soup.find_all(name = 'h3',class_='title')
-
 movies_titles = [movie.getText() for movie in movies_list]
+movies_titles.reverse()
 
-print(movies_titles)
+print(type(movies_titles[0]))
+
+
+file = open('45_day_Parsing_HTML_Making_Soup/2_project/movies_list.txt', 'w')
+
+with open ('45_day_Parsing_HTML_Making_Soup/2_project/movies_list.txt', 'w') as file:
+
+    for item in movies_titles:
+        file.write(item + '\n')
+    file.close()
+
+
+
+# for item in movies_titles:
+#     file.write(item + '\n')
+# file.close()
+
+
+
+
+
